b0fa8ff74248ac8170d87b9511268ad4d193c7de
mwchoen <cheonmyeongwon@gmail.com>
2023-05-19 21:30:08 +0900

    todo 만들기

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import AppMy from "./App-My";
+
+
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<AppMy />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/components/todolist/TodoList.jsx
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+import AddTodo from "../addTodo/AddTodo";
+
+export default function TodoList() {
+    const [todos, setTodos] = useState([
+        { id: 1, text:'장보기', status: 'active' },
+        { id: 2, text:'공부하기', status: 'active' }
+    ])
+
+    const handleAdd = (todo) => setTodos([...todos, todo]);
+
+    return (
+        <section>
+            <ul>
+                {
+                    todos.map(item => <li key={item.id}> {item.text} </li>)
+                }
+            </ul>
+            <AddTodo onAdd = {handleAdd} />
+        </section>
+    )
+
+};
